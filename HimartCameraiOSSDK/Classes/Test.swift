6056a0b7ba3427c6c39ab8b76307fea5a50a3f0d
Seungjung <sngsng@slogup.com>
2017-03-20 12:10:03 +0900

    feat : Add test functionality

---
+++ HimartCameraiOSSDK/Classes/Test.swift
@@ -0,0 +1,17 @@
+//
+//  Test.swift
+//  Pods
+//
+//  Created by 김승중 on 2017. 3. 20..
+//
+//
+
+import Foundation
+
+class Test {
+    
+    class func log(msg:String) {
+        
+        Swift.debugPrint(msg)
+    }
+}
